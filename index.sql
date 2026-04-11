a34e97a81c65e343dae4d475752b115e1d1520c7
SV <vsbabu@gmail.com>
2026-04-11 16:18:32 +0530

    Used redirect to get rid of complex processing for default values of date ranges

--- index.sql
+++ index.sql
@@ -1,3 +1,19 @@
+-- if no start date is passed, assume it is home page and redirect to current month
+SELECT
+    'redirect' AS component,
+    '/?t='
+    || strftime('%Y-%m', current_date, 'start of month')
+    || '&start='
+    || date(current_date, 'start of month')
+    || '&end='
+    || current_date
+    || '&pstart='
+    || date(current_date, 'start of month', '-1 months')
+    || '&pend='
+    || date(current_date,  '-1 months')
+    AS link
+WHERE $start is null;
+
 SELECT 'dynamic' AS component,
 	sqlpage.run_sql('shell.sql')
 	AS properties;
@@ -6,18 +22,18 @@ SELECT 'dynamic' AS component,
 SELECT 'dynamic' AS component,
   sqlpage.run_sql('search_results.sql',
     json_object(
-        't', iif($start is null,strftime('%Y-%m', 'now'), ifnull($t, $start||' - '||$end)),
-        'start', ifnull($start, date('now','start of month')),
+        't', ifnull($t, $start||' - '||$end),
+        'start', $start,
         'end', ifnull($end, current_date),
         'category',ifnull($category,''),
         'exclude', ifnull($exclude,''),
         'payee', ifnull($payee, ''),
         'datagrid', ifnull($datagrid,''),
-        -- NOTE This default calculation is incorrect in many instances. That is why this is calculated in  previous_range from menu
-        -- and passed from query string to index.sql to here. During custom search, these will be null and a best effort case is handled for ifnull.
-        -- It will also me null for homepage landing which is current month to current date. That is the condition coded here for full previous month.
-        'pstart', ifnull($pstart, date(ifnull($start, date('now','start of month')), concat('-',(julianday(ifnull($end, current_date))-julianday(ifnull($start, date('now','start of month'))))/30,' months'), 'start of month')),
-        'pend', ifnull($pend,date(ifnull($start, date('now','start of month')), '-1 days'))
+        -- this pstart and pend being null will only happen when search from the form. 
+        -- in that situation, use default: pstart = start - (end-start days)
+        'pstart', ifnull($pstart,
+               date($start, concat('-',julianday(ifnull($end, current_date))-julianday($start),' days'))),
+        'pend', ifnull($pend,date($start,  '-1 days'))
       )
   )
 	AS properties;
