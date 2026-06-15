81bfc3c5abc0b08a2cdc203d805f455cd2e7af03
SV <vsbabu@gmail.com>
2026-06-15 17:08:47 +0530

    Added colors by hard coded series to treemap

--- r_net_chart_by_category_treemap.sql
+++ r_net_chart_by_category_treemap.sql
@@ -6,11 +6,20 @@ select
     TRUE      as labels;
 
 -- TODO: Add a link to category filter query in values. sqlchart doesn't support this yet.
+-- FIXME: Categories should've a lookup value for grouping in db instead of this CASE statement
+--        The split of series adds colors to the treemap
 SELECT
     category as label,
+    CASE
+        WHEN category IN ('Tax', 'TDS') THEN 'Government'
+        WHEN category IN ('Hire', 'Fuel', 'Grocery', 'Dinner', 'Telephone', 'Medicine') THEN 'Recurring'
+        WHEN category IN ('Car', 'Bike') THEN 'Vehicles'
+        WHEN category IN ('School','Fun', 'Gifts', 'Clothes') THEN 'Often'
+        ELSE 'Others'
+    END AS series,
     cast(sum(net)/1000 as integer) AS y
 FROM filtered
 where category not in ('Transfer', 'Reconcile', 'Salary', 'Interest')
-GROUP BY 1
+GROUP BY 1, 2
 ORDER BY 1 asc
 ;
